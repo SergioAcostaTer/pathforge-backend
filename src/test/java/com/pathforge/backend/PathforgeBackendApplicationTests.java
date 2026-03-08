d7bbc327592ea1992853edee6ecff3e4e55076cf
Sergio Acosta <sergioacostaquintana@gmail.com>
2026-03-08 22:43:59 +0000

    feat: initialize spring boot project structure

---
+++ src/test/java/com/pathforge/backend/PathforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathforge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
